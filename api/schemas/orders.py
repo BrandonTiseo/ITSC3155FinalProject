74ef84e02a402ac61e907689fb632831047dc913
Sean Devlin <sean.dev224@gmail.com>
2025-04-09 14:39:10 -0400

    linked orders with order details

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -4,21 +4,26 @@ from pydantic import BaseModel
 from .order_details import OrderDetail
 
 
-
+#basic version of Order, containing only the values we might need to update later
 class OrderBase(BaseModel):
     customer_name: str
     description: Optional[str] = None
+    status: str
+    
 
 
 class OrderCreate(OrderBase):
-    pass
+    order_details:list[int] = None
+    item_amounts: list[int] = None
 
 
 class OrderUpdate(BaseModel):
     customer_name: Optional[str] = None
     description: Optional[str] = None
+    status: Optional[str] = None
 
 
+#full version of Order with all information
 class Order(OrderBase):
     id: int
     order_date: Optional[datetime] = None
